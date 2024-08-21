edbab85af5b8fbe91200be70e0019bd3f88f0028
Chris Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-08-22 01:41:56 +0200

    save

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controllers/post.js";
+import { addPosts, getPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", addPosts);
 
 export default router;
